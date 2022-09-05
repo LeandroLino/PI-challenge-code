b874606f0a86d43228432a1b53182e24484d61f3
LeandroLino <leao.lino7@gmail.com>
2022-09-05 20:17:38 -0300

    FIX :: With prime nubmers now

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PI import PI #File with 1M of digits
+from PI import PI
 from pythonds.basic.deque import Deque
+import math
+import requests
 
 def is_palindromes(aString):
     chardeque = Deque()
@@ -17,18 +19,29 @@ def is_palindromes(aString):
 
     return stillEqual
 
+def is_prime(seqNum):
+    n = int(seqNum)
+    for i in range(2,int(math.sqrt(n))+1):
+        if (n%i) == 0:
+            return False
+    return True
+
 def find_palindromes_in_pi(palindromeSize):
     palindrome = ''
-    for n in PI[2:]:
+    for _, n in enumerate(PI):
         if(len(palindrome)==palindromeSize):
             palindrome = palindrome[1:]
             palindrome = palindrome + n
         elif(len(palindrome)<palindromeSize):
             palindrome = palindrome + n
 
-        if(is_palindromes(palindrome) and len(palindrome) == palindromeSize):
+        if(is_palindromes(palindrome)
+        and len(palindrome) == palindromeSize 
+        and is_prime(palindrome)
+        ):  
             print(f'First Palindrome with {palindromeSize} digits:', palindrome)
             break
 
 if __name__ == "__main__":
-    find_palindromes_in_pi(9)
+    find_palindromes_in_pi(21)
+    #pass
